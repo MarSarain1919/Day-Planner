06b6f64527e2deacdb40f36c942fdea1c7871a55
Katja Sarain <katjasarain@gmail.com>
2021-04-09 23:19:42 -0700

    third commit. followed along a demo and cant figure out why save icon does not appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,185 @@
-
-// Live date and time count
-function display_ct7() {
-    var x = new Date()
-    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-    hours = x.getHours( ) % 12;
-    hours = hours ? hours : 12;
-    hours=hours.toString().length==1? 0+hours.toString() : hours;
-    
-    var minutes=x.getMinutes().toString()
-    minutes=minutes.length==1 ? 0+minutes : minutes;
-    
-    var seconds=x.getSeconds().toString()
-    seconds=seconds.length==1 ? 0+seconds : seconds;
-    
-    var month=(x.getMonth() +1).toString();
-    month=month.length==1 ? 0+month : month;
-    
-    var dt=x.getDate().toString();
-    dt=dt.length==1 ? 0+dt : dt;
-    
-    var x1=month + "/" + dt + "/" + x.getFullYear(); 
-    x1 = x1 + " - " +  hours + ":" +  minutes + ":" +  seconds + " " + ampm;
-    document.getElementById('date').innerHTML = x1;
-    display_c7();
-     }
-     function display_c7(){
-    var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_ct7()',refresh)
+$(document).ready(function() {
+  
+    // test flag
+    const test = false;
+  
+    // get times from moment
+    const now = moment().format('MMMM Do YYYY');
+  
+    // commented out for test in non-standard hours
+    let nowHour24 = moment().format('H');
+    let nowHour12 = moment().format('h');
+  
+    // set times for tesitng after hours
+    if (test) {
+      nowHour24 = 13;
+      nowHour12 = 1;
     }
-    display_c7()
+  
+    let $dateHeading = $('#navbar-subtitle');
+    $dateHeading.text(now);
+ 
+  
+    // Get stored todos from localStorage
+    // Parsing the JSON string to an object
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+  
+    if (test) { console.log(storedPlans); }
+  
+    // If plans were retrieved from localStorage, update the plan array to it
+    if (storedPlans !== null) {
+      planTextArr = storedPlans;
+    } else {
+      // this should only occur on first time the app is loaded in the browser
+      // helpfully remind user that lunch is important
+      planTextArr = new Array(9);
+      planTextArr[4] = "Meeting with boss";
+    }
+  
+    if (test) { console.log("full array of plned text",planTextArr); }
+  
+    // set variable referencing planner element
+    let $plannerDiv = $('#plannerContainer');
+    // clear existing elements
+    $plannerDiv.empty();
+  
+    if (test) { console.log("current time",nowHour12); }
+  
+  
+    // build calendar by row for fix set of hours
+    for (let hour = 9; hour <= 17; hour++) {
+      // index for array use offset from hour
+      let index = hour - 9;
+      
+      // build row components
+      let $rowDiv = $('<div>');
+      $rowDiv.addClass('row');
+      $rowDiv.addClass('plannerRow');
+      $rowDiv.attr('hour-index',hour);
+    
+      // Start building Time box portion of row
+      let $col2TimeDiv = $('<div>');
+      $col2TimeDiv.addClass('col-md-2');
+    
+      // create timeBox element (contains time)
+      const $timeBoxSpn = $('<span>');
+      // can use this to get value
+      $timeBoxSpn.attr('class','timeBox');
+      
+      // format hours for display
+      let displayHour = 0;
+      let ampm = "";
+      if (hour > 12) { 
+        displayHour = hour - 12;
+        ampm = "pm";
+      } else {
+        displayHour = hour;
+        ampm = "am";
+      }
+      
+      // populate timeBox with time
+      $timeBoxSpn.text(`${displayHour} ${ampm}`);
+  
+      // insert into col inset into timebox
+      $rowDiv.append($col2TimeDiv);
+      $col2TimeDiv.append($timeBoxSpn);
+      // STOP building Time box portion of row
+  
+      // START building input portion of row
+      // build row components
+      let $dailyPlanSpn = $('<input>');
+  
+      $dailyPlanSpn.attr('id',`input-${index}`);
+      $dailyPlanSpn.attr('hour-index',index);
+      $dailyPlanSpn.attr('type','text');
+      $dailyPlanSpn.attr('class','dailyPlan');
+  
+      // access index from data array for hour 
+      $dailyPlanSpn.val( planTextArr[index] );
+      
+      // create col to control width
+      let $col9IptDiv = $('<div>');
+      $col9IptDiv.addClass('col-md-9');
+  
+      // add col width and row component to row
+      $rowDiv.append($col9IptDiv);
+      $col9IptDiv.append($dailyPlanSpn);
+      // STOP building Time box portion of row
+  
+      // START building save portion of row
+      let $col1SaveDiv = $('<div>');
+      $col1SaveDiv.addClass('col-md-1');
+  
+      let $saveBtn = $('<p>');
+      $saveBtn.attr('id',`saveid-${index}`);
+      $saveBtn.attr('save-id',index);
+      $saveBtn.attr('class',"saveIcon");
+      
+      // add col width and row component to row
+      $rowDiv.append($col1SaveDiv);
+      $col1SaveDiv.append($saveBtn);
+      // STOP building save portion of row
+  
+      // set row color based on time
+      updateRowColor($rowDiv, hour);
+      
+      // add row to planner container
+      $plannerDiv.append($rowDiv);
+    };
+  
+    // function to update row color
+    function updateRowColor ($hourRow,hour) { 
+  
+      if (test) { console.log("rowColor ",nowHour24, hour); }
+  
+      if ( hour < nowHour24) {
+        // $hourRow.css('')
+        if (test) { console.log("lessThan"); }
+        $hourRow.css("background-color","lightgrey")
+      } else if ( hour > nowHour24) {
+        if (test) { console.log("greaterthan"); }
+        $hourRow.css("background-color","green")
+      } else {
+        if (test) { console.log("eqaul"); }
+        $hourRow.css("background-color","red")
+      }
+    };
+  
+    // saves to local storage
+    // conclick function to listen for user clicks on plan area
+    $(document).on('click','p', function(event) {
+      event.preventDefault();  
+  
+      if (test) { console.log('click pta before '+ planTextArr); }
+  
+      let $index = $(this).attr('save-id');
+  
+      let inputId = '#input-'+$index;
+      let $value = $(inputId).val();
+  
+      planTextArr[$index] = $value;
+  
+  
+      if (test) { console.log('value ', $value); }
+      if (test) { console.log('index ', $index); }
+      if (test) { console.log('click pta after '+ planTextArr); }
+  
+      // remove shawdow pulse class
+      $(`#saveid-${$index}`).removeClass('shadowPulse');
+      localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+    });  
+    
+    // function to color save button on change of input
+    $(document).on('change','input', function(event) {
+      event.preventDefault();  
+      if (test) { console.log('onChange'); }
+      if (test) { console.log('id', $(this).attr('hour-index')); }
+  
+      // neeed to check for save button
+  
+      let i = $(this).attr('hour-index');
+  
+      // add shawdow pulse class
+      $(`#saveid-${i}`).addClass('shadowPulse');
+    });
+  });
